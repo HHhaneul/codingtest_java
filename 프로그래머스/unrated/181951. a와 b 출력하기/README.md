1a54a7d922e8c2eaa168645f16f89d2d295fe646
HHhaneul <137499822+HHhaneul@users.noreply.github.com>
2023-07-04 15:19:02 +0900

    [unrated] Title: a와 b 출력하기, Time: 135.40 ms, Memory: 60.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181951. a와 b 출력하기/README.md
+++ 프로그래머스/unrated/181951. a와 b 출력하기/README.md
@@ -1,10 +1,10 @@
 # [unrated] a와 b 출력하기 - 181951 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181951) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181951?language=java) 
 
 ### 성능 요약
 
-메모리: 72.4 MB, 시간: 302.64 ms
+메모리: 60.9 MB, 시간: 135.40 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181951. a와 b 출력하기/a와 b 출력하기.java
+++ 프로그래머스/unrated/181951. a와 b 출력하기/a와 b 출력하기.java
@@ -3,18 +3,9 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String a = sc.next();
-        String b = sc.next();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
-        String str = String.valueOf(new StringBuffer(1000000));
-
-        str = a;
-
-        String str2 = String.valueOf(new StringBuffer(1000000));
-
-        str2 = b;
-
-        System.out.printf("a = %s%n", str);
-        System.out.printf("b = %s", str2);
+        System.out.printf("a = %d%nb = %d", a, b);
     }
 }
